a92ed9cd516dc5cc10b9e96af71b4c7436b48a1a
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-02-03 21:18:55 -0500

    Preview Card for the small devices

--- src/components/PreviewCard.js
+++ src/components/PreviewCard.js
@@ -8,55 +8,57 @@ function PreviewCard() {
   return (
     <section id="preview-card">
       <div className="container">
-        <div className="item left">
-          <div className="img-wrapper">
-            <img src={sedanImg} alt="" />
+        <div className="grid-container">
+          <div className="item left">
+            <div className="img-wrapper">
+              <img src={sedanImg} alt="" />
+            </div>
+            <div>
+              <h3>SEDANS</h3>
+            </div>
+            <div className="">
+              <p>
+                Choose a sedan for its affordability and excellent fuel economy.
+                Ideal for cruising in the city or an your next road trip
+              </p>
+            </div>
+            <div>
+              <button>Learn More</button>
+            </div>
           </div>
-          <div>
-            <h3>SEDANS</h3>
+          <div className="item middle">
+            <div className="img-wrapper">
+              <img src={suvsImg} alt="" />
+            </div>
+            <div>
+              <h3>SEDANS</h3>
+            </div>
+            <div>
+              <p>
+                Take an SUV for its spacious interior, power, and versatility.
+                Perfect for your next family vacation and off-road adventures.
+              </p>
+            </div>
+            <div>
+              <button>Learn More</button>
+            </div>
           </div>
-          <div className="">
-            <p>
-              Choose a sedan for its affordability and excellent fuel economy.
-              Ideal for cruising in the city or an your next road trip
-            </p>
-          </div>
-          <div>
-            <button>Learn More</button>
-          </div>
-        </div>
-        <div className="item middle">
-          <div className="img-wrapper">
-            <img src={suvsImg} alt="" />
-          </div>
-          <div>
-            <h3>SEDANS</h3>
-          </div>
-          <div>
-            <p>
-              Take an SUV for its spacious interior, power, and versatility.
-              Perfect for your next family vacation and off-road adventures.
-            </p>
-          </div>
-          <div>
-            <button>Learn More</button>
-          </div>
-        </div>
-        <div className="item right">
-          <div className="img-wrapper">
-            <img src={luxuryImg} alt="" />
-          </div>
-          <div>
-            <h3>LUXURY</h3>
-          </div>
-          <div className="">
-            <p>
-              Cruise in the best car brands without the bloated prices. Enjoy
-              the enhanced comfort of a luxury rental and arrive in style.
-            </p>
-          </div>
-          <div>
-            <button>Learn More</button>
+          <div className="item right">
+            <div className="img-wrapper">
+              <img src={luxuryImg} alt="" />
+            </div>
+            <div>
+              <h3>LUXURY</h3>
+            </div>
+            <div className="">
+              <p>
+                Cruise in the best car brands without the bloated prices. Enjoy
+                the enhanced comfort of a luxury rental and arrive in style.
+              </p>
+            </div>
+            <div>
+              <button>Learn More</button>
+            </div>
           </div>
         </div>
       </div>
