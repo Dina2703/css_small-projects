588a03b55d34156ddf7d7b82fcb90f6da1b1061d
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-02-05 16:25:04 -0500

    Price-Grid: style for small devices

--- src/components/PriceGrid.js
+++ src/components/PriceGrid.js
@@ -10,8 +10,8 @@ function PriceGrid() {
           <p>30-day, hassle-free money back guarantee</p>
           <p>
             Gain access to our full library of tutorials along with expert code
-            reviews. <br /> Perfect for any developers who are serious about
-            honing theri skills.
+            reviews.Perfect for any developers who are serious about honing
+            theri skills.
           </p>
         </div>
         <div className="grid-container">
