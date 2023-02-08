a3d7f3b26ed2b7bc22a740b9b93790fd0575ae41
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-02-08 09:31:29 -0500

    Huddle Landing Page: HTML structure, and setting assets

--- src/components/HuddlePage.js
+++ src/components/HuddlePage.js
@@ -1,12 +1,53 @@
 import React from "react";
 
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import logo from "../assets/huddle-landing/images/logo.svg";
+import mockups from "../assets/huddle-landing/images/illustration-mockups.svg";
+
 function HuddlePage() {
   return (
     <div id="huddle">
       <div className="huddle-container">
-        <div>
-          <img src="../assets/huddle-landing/images/logo.svg" alt="" />
-        </div>
+        <header>
+          <div className="logo-wrapper">
+            <img src={logo} alt="huddle logo" />
+          </div>
+        </header>
+        <main>
+          <div className="hero-image">
+            <img src={mockups} alt="mockups" />
+          </div>
+          <div className="hero-context-wrapper">
+            <div className="title">
+              <h1>Build The Community</h1>
+              <h1>Your Fans Will Love</h1>
+            </div>
+            <div>
+              <p>
+                Huddle re-imagines the way we build communities. You have a
+                voice, but so does your audience. Create connections with your
+                users as you engage in genuine discussion.
+              </p>
+            </div>
+          </div>
+        </main>
+        <footer>
+          <div>
+            <ul>
+              <li>
+                <FaFacebookF />
+              </li>
+              <li>
+                <FaTwitter />
+              </li>
+              <li>
+                <FaInstagram />
+              </li>
+            </ul>
+          </div>
+        </footer>
       </div>
       <h3>Huddle landing page with a single introductory section</h3>
     </div>
