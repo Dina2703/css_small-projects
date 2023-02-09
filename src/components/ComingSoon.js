6c0254470283de31cff097296d91b156dded1497
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-02-08 20:18:19 -0500

    coming soon project: email form validation

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import "../style/coming_soon.css";
 import heroImg from "../assets/coming-soon/images/illustration-dashboard.png";
 import PingLogo from "../assets/coming-soon/images/logo.svg";
@@ -7,6 +7,26 @@ import { FaTwitter } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 
 function ComingSoon() {
+  const [err, setErr] = useState(false);
+  const [input, setInput] = useState("");
+
+  // console.log(input);
+  function handleChange(e) {
+    setInput((prev) => e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (input.match(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g)) {
+      setErr(false);
+      console.log("Success");
+      setInput("");
+    } else {
+      setInput("");
+      setErr(true);
+    }
+  }
+
   return (
     <div id="coming-soon">
       <div className="container">
@@ -17,10 +37,25 @@ function ComingSoon() {
           We are launching <span>soon!</span>
         </div>
         <p>Subscribe and get notified</p>
-        <div className="notify_wrapper">
-          <input type="text" placeholder="Your email address..." />{" "}
-          <button>Notify Me</button>
-        </div>
+        <form className="notify_wrapper" onSubmit={handleSubmit}>
+          <div>
+            <input
+              className={err ? "err" : ""}
+              type="text"
+              placeholder={err ? "example@email.com" : "Your email address..."}
+              value={input}
+              onChange={handleChange}
+            />
+            <p
+              className="err-message"
+              style={{ display: err ? "block" : "none" }}
+            >
+              Please Provide a valid email address
+            </p>
+          </div>
+
+          <button type="submit">Notify Me</button>
+        </form>
         <div className="img_wrapper">
           <img src={heroImg} alt="" />
         </div>
