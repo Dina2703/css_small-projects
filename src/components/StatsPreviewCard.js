5cf30542e2da9366b462f9cf4533a5ebee1a8210
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-02-09 20:06:54 -0500

    Stats preview project starter: html structure, hero-img layering

--- src/components/StatsPreviewCard.js
+++ src/components/StatsPreviewCard.js
@@ -1,9 +1,36 @@
 import React from "react";
+import "../style/stats_preview.css";
+import heroImg from "../assets/stats-preview/images/image-header-desktop.jpg";
 
 function StatsPreviewCard() {
   return (
-    <div>
-      <h3>Stats preview card component</h3>
+    <div id="stats_preview">
+      <div className="container flex">
+        <div className="hero_img_wrapper"></div>
+        <div className="context_container">
+          <h2>
+            Get <span>insights</span> that help your business grow.
+          </h2>
+          <p>
+            Discover the benefits of data analitics and make better decisions
+            regarding revenue, customer experience, and overall efficiency.
+          </p>
+          <div className="flex stats">
+            <div className="stat">
+              <span>10k+</span>
+              <span>companies</span>
+            </div>
+            <div className="stat">
+              <span>314 </span>
+              <span>templates</span>
+            </div>
+            <div className="stat">
+              <span>12M+</span>
+              <span>queries</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
