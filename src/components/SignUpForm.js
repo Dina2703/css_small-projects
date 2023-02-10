8a303a73847779e09324400ac277f8fa5cbfec9b
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-02-09 22:50:09 -0500

    sign_up_form project starter

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,9 +1,20 @@
 import React from "react";
+import "../style/sign_up_form.css";
 
 function SignUpForm() {
   return (
-    <div>
-      <h3>Intro component with sign-up form</h3>
+    <div id="sign_up_form" className="flex">
+      <div className="container flex">
+        <div className="hero_leftside">
+          <h2>Learn to code by watching others </h2>
+          <p>
+            See how experienced developers solve problems in real-time. Watching
+            scripted tutorials is great, but understanding how deveolers think
+            is invaluable.
+          </p>
+        </div>
+        <div className="form_container"></div>
+      </div>
     </div>
   );
 }
